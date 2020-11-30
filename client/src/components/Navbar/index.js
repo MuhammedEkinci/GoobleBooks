c94a8c523141e97680296c529a827b460c3c616e
Muhammed Ekinci <siraceddinekinci@gmail.com>
2020-11-30 02:56:25 -0500

    navbar appears on react page

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">Google Books</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                    <a className="nav-link" href="/Books">Search</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/saved">Saved</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    );
+}
+
+export default NavBar;
